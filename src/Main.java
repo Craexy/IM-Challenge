56522b3312a1041cecfc599bfd612c10d39c4d9e
Philipp <phildersiebers@gmx.de>
2017-09-26 14:37:25 +0200

    Production for one car and adjustments to time class

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,10 @@ public class Main {
 	
 	
 	public static void main(String[] args) {
-
-		
-	Time zeit1 = new Time(8,20,0);	
-	Time zeit2 = new Time(7,50,0);
 	
-	System.out.println(zeit2.compareTo(zeit1));
+	Tourenplaner tourp = new Tourenplaner();
+	Produktionsplaner produktionp = new Produktionsplaner(tourp.getFahrzeuge());
 	
-	
-	
-		
-	Tourenplaner tour = new Tourenplaner();
-	System.out.println(tour.getFahrzeuge().get(0).getStrecke());
-	//Routenplaner route = new Routenplaner();
-
 	
 	}
 
--- src/Time.java
+++ src/Time.java
@@ -15,15 +15,18 @@ public class Time implements Comparable<Time>{
 		if (Minuten<0) throw new IllegalArgumentException();
 		if (this.minuten>=Minuten) this.minuten = this.minuten - Minuten; 
 		else {
-			this.stunden = this.stunden - Minuten/60 - 1;
-			this.minuten = this.minuten + 60 - Minuten%60;
+			if (Minuten%60==0){ this.stunden = this.stunden-Minuten/60;
+								
+			}
+			else {this.stunden = this.stunden - Minuten/60 - 1;
+			this.minuten = this.minuten + 60 - Minuten%60;}
 			if (this.minuten==60) {
 				minuten = 0;
 				stunden = stunden + 1;
 			}
 		}
 		return new Time(stunden, minuten, sekunden);
-	}
+		}
 	
 	public Time addTime(int Minuten) {
 		if (Minuten<=60-minuten) minuten = minuten + Minuten;
