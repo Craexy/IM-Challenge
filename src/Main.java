4815536f3f256369989c9cf0dba86a17657f6d8b
Craexy <christopher.utsch@gmail.com>
2017-10-03 11:38:19 +0200

    Tourenplaner more dynamic

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
 	public static void main(String[] args) {
 		
 	Tourenplaner tour = new Tourenplaner();
-	System.out.println(tour.getFahrzeuge().get(1).getStrecke());
 	//Routenplaner route = new Routenplaner();
 
 	
